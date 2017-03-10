8818b3e1c168184b88320d817c01bb3bff32da57
okoala <dapixp@gmail.com>
2017-03-10 19:19:16 +0800

    feat: support multi robot.

--- test/dingtalkRobot.test.js
+++ test/dingtalkRobot.test.js
@@ -5,40 +5,68 @@ const request = require('supertest');
 const mm = require('egg-mock');
 
 describe('test/dingtalkRobot.test.js', () => {
-  let app;
+  describe('single robot', () => {
+    let app;
 
-  before(function* () {
-    app = mm.app({
-      baseDir: 'robot',
+    before(function* () {
+      app = mm.app({
+        baseDir: 'robot',
+      });
+      yield app.ready();
     });
-    yield app.ready();
+
+    afterEach(mm.restore);
+
+    it('app.dingtalkRobot', function* () {
+      assert(app.dingtalkRobot);
+    });
+
+    it('should sendText success', function(done) {
+      request(app.callback())
+        .get('/sendText')
+        .expect(200)
+        .end(done);
+    });
+
+    it('should sendLink success', function(done) {
+      request(app.callback())
+        .get('/sendLink')
+        .expect(200)
+        .end(done);
+    });
+
+    it('should sendMarkdown success', function(done) {
+      request(app.callback())
+        .get('/sendMarkdown')
+        .expect(200)
+        .end(done);
+    });
   });
 
-  afterEach(mm.restore);
+  describe('multiple robot', () => {
+    let app;
 
-  it('app.dingtalkRobot', function* () {
-    assert(app.dingtalkRobot);
+    before(function* () {
+      app = mm.app({
+        baseDir: 'robot-multiple',
+      });
+      yield app.ready();
+    });
+
+    afterEach(mm.restore);
+
+    it('should multi sendText success', function(done) {
+      request(app.callback())
+        .get('/multi/sendText')
+        .expect(200)
+        .end(done);
+    });
+
+    it('should multi sendLink success', function(done) {
+      request(app.callback())
+        .get('/multi/sendLink')
+        .expect(200)
+        .end(done);
+    });
   });
-
-  it('should sendText success', function(done) {
-    request(app.callback())
-      .get('/sendText')
-      .expect(200)
-      .end(done);
-  });
-
-  it('should sendLink success', function(done) {
-    request(app.callback())
-      .get('/sendLink')
-      .expect(200)
-      .end(done);
-  });
-
-  it('should sendMarkdown success', function(done) {
-    request(app.callback())
-      .get('/sendMarkdown')
-      .expect(200)
-      .end(done);
-  });
-
 });
---
+++ test/fixtures/robot-multiple/config/config.default.js
@@ -0,0 +1,14 @@
+exports.keys = '12234';
+
+exports.dingtalkRobot = {
+  robots: {
+    testRobot1: {
+      name: '测试一号',
+      accessToken: 'e5a51f835a279853d7965710c1b2b4fc93e8ea95ae17ed3bff16cd01a75a14dd'
+    },
+    testRobot2: {
+      name: '测试二号',
+      accessToken: 'cd0b571b586712c488894ae8963b01821a5dc1a406a3506cf473e93b0170e14f'
+    }
+  }
+};
